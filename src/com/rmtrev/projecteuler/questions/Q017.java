f2d9ebdf7dd6ad9f23239bc297a027a1a0b067d8
Trevor Daniels <rm.trev@gmail.com>
2016-02-03 21:07:40 -0600

    P 17: Changed int literals to named constants to improve readability.

--- src/com/rmtrev/projecteuler/questions/Q017.java
+++ src/com/rmtrev/projecteuler/questions/Q017.java
@@ -3,6 +3,41 @@ package com.rmtrev.projecteuler.questions;
 public class Q017 implements IQuestion
 {
 	private static int NUMBER = 1000;
+	
+	private static int LEN_AND = 3;
+	
+	private static int LEN_ONE = 3;
+	private static int LEN_TWO = 3;
+	private static int LEN_THREE = 5;
+	private static int LEN_FOUR = 4;
+	private static int LEN_FIVE = 4;
+	private static int LEN_SIX = 3;
+	private static int LEN_SEVEN = 5;
+	private static int LEN_EIGHT = 5;
+	private static int LEN_NINE = 4;
+	
+	private static int LEN_TEN = 3;
+	private static int LEN_ELEVEN = 6;
+	private static int LEN_TWELVE = 6;
+	private static int LEN_THIRTEEN = 8;
+	private static int LEN_FOURTEEN = 8;
+	private static int LEN_FIFTEEN = 7;
+	private static int LEN_SIXTEEN = 7;
+	private static int LEN_SEVENTEEN = 9;
+	private static int LEN_EIGHTEEN = 8;
+	private static int LEN_NINETEEN = 8;
+	
+	private static int LEN_TWENTY = 6;
+	private static int LEN_THIRTY = 6;
+	private static int LEN_FORTY = 5;
+	private static int LEN_FIFTY = 5;
+	private static int LEN_SIXTY = 5;
+	private static int LEN_SEVENTY = 7;
+	private static int LEN_EIGHTY = 6;
+	private static int LEN_NINETY = 6;
+	
+	private static int LEN_HUNDRED = 7;
+	private static int LEN_THOUSAND = 8;
 
 	@Override
 	public void Run(String[] args) throws Exception
@@ -27,7 +62,7 @@ public class Q017 implements IQuestion
 		
 		if(strNum.length() == 4)
 		{
-			ret += getNumSingle(strNum.charAt(0)) + 8; // thousand = 8
+			ret += getNumSingle(strNum.charAt(0)) + LEN_THOUSAND;
 			nextIndex++;
 		}
 		
@@ -36,7 +71,7 @@ public class Q017 implements IQuestion
 			char c = strNum.charAt(nextIndex);
 			if(c != '0')
 			{
-				ret += getNumSingle(strNum.charAt(nextIndex)) + 7; // hundred = 7
+				ret += getNumSingle(strNum.charAt(nextIndex)) + LEN_HUNDRED;
 				hasHundred = true;
 			}
 			nextIndex++;
@@ -48,7 +83,7 @@ public class Q017 implements IQuestion
 			char ones = strNum.charAt(nextIndex+1);
 			if(hasHundred && (tens != '0' || ones != '0'))
 			{
-				ret += 3; // and = 3
+				ret += LEN_AND;
 			}
 			ret += getNumPair(tens, ones);
 			nextIndex += 2;
@@ -59,7 +94,7 @@ public class Q017 implements IQuestion
 			char c = strNum.charAt(nextIndex);
 			if(hasHundred && c != '0')
 			{
-				ret += 3 ; // and = 3
+				ret += LEN_AND;
 			}
 			ret += getNumSingle(c);
 		}
@@ -73,23 +108,23 @@ public class Q017 implements IQuestion
 		switch(i)
 		{
 		case 1:
-			return 3; // "one";
+			return LEN_ONE;
 		case 2:
-			return 3; // "two";
+			return LEN_TWO;
 		case 3:
-			return 5; // "three";
+			return LEN_THREE;
 		case 4:
-			return 4; // "four";
+			return LEN_FOUR;
 		case 5:
-			return 4; // "five";
+			return LEN_FIVE;
 		case 6:
-			return 3; // "six";
+			return LEN_SIX;
 		case 7:
-			return 5; // "seven";
+			return LEN_SEVEN;
 		case 8:
-			return 5; // "eight";
+			return LEN_EIGHT;
 		case 9:
-			return 4; // "nine";
+			return LEN_NINE;
 		default:
 			return 0;
 		}
@@ -106,25 +141,25 @@ public class Q017 implements IQuestion
 			switch(ones)
 			{
 			case '0':
-				return 3; // "ten";
+				return LEN_TEN;
 			case '1':
-				return 6; // "eleven";
+				return LEN_ELEVEN;
 			case '2':
-				return 6; // "twelve";
+				return LEN_TWELVE;
 			case '3':
-				return 8; // "thirteen";
+				return LEN_THIRTEEN;
 			case '4':
-				return 8; // "fourteen";
+				return LEN_FOURTEEN;
 			case '5':
-				return 7; // "fifteen";
+				return LEN_FIFTEEN;
 			case '6':
-				return 7; // "sixteen";
+				return LEN_SIXTEEN;
 			case '7':
-				return 9; // "seventeen";
+				return LEN_SEVENTEEN;
 			case '8':
-				return 8; // "eighteen";
+				return LEN_EIGHTEEN;
 			case '9':
-				return 8; // "nineteen";
+				return LEN_NINETEEN;
 			default:
 				return 0;
 			}
@@ -134,21 +169,21 @@ public class Q017 implements IQuestion
 			switch(tens)
 			{
 			case '2':
-				return 6 + getNumSingle(ones);
+				return LEN_TWENTY + getNumSingle(ones);
 			case '3':
-				return 6 + getNumSingle(ones);
+				return LEN_THIRTY + getNumSingle(ones);
 			case '4':
-				return 5 + getNumSingle(ones);
+				return LEN_FORTY + getNumSingle(ones);
 			case '5':
-				return 5 + getNumSingle(ones);
+				return LEN_FIFTY + getNumSingle(ones);
 			case '6':
-				return 5 + getNumSingle(ones);
+				return LEN_SIXTY + getNumSingle(ones);
 			case '7':
-				return 7 + getNumSingle(ones);
+				return LEN_SEVENTY + getNumSingle(ones);
 			case '8':
-				return 6 + getNumSingle(ones);
+				return LEN_EIGHTY + getNumSingle(ones);
 			case '9':
-				return 6 + getNumSingle(ones);
+				return LEN_NINETY + getNumSingle(ones);
 			default:
 				return 0;
 			}
